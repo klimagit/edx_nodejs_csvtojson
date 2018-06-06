f17a58b6e5450a2c14bdb541d1fb376d23fcfba5
klimagit <39964957+klimagit@users.noreply.github.com>
2018-06-06 23:12:16 +0200

    Update csv_json_2.js

--- csv_json_2.js
+++ csv_json_2.js
@@ -5,5 +5,5 @@ const csv = require('csvtojson');
 csv()
 .fromFile(path.join(__dirname, 'customer-data.csv'))
 .then((jsonObj)=>{
- fs.writeFile (path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj), function(err) {if (err) throw err;});
+ fs.writeFile (path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj,null,2), function(err) {if (err) throw err;});
 })
